e958f0026f3014a846dc0b9db734de88f1200ae8
JohanHaas <johanhaas99@gmail.com>
2025-08-04 13:39:31 +0200

    rustBar

---
+++ shells/rustBar/flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "rustBar shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          pkgsUnstable = import nixpkgs-unstable { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+
+            packages = with pkgsUnstable; [
+              rustc
+              cargo
+              rustfmt
+              clippy
+
+              pkg-config
+              gtk4
+              libadwaita
+              wayland
+              wlroots
+
+              cmake
+              ninja
+              clang-tools
+            ];
+
+            shellHook = ''
+              echo ""
+              echo "you have entered rustBar Devshell"
+            '';
+          };
+        }
+      );
+    };
+}
