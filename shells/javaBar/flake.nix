492bc499da1bb7f15403418709370962c4fc109b
JohanHaas <johanhaas99@gmail.com>
2025-08-11 11:52:44 +0200

    javaBar

---
+++ shells/javaBar/flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "devshell for javaBar";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          pkgsUnstable = import nixpkgs-unstable { inherit system; };
+        in
+        {
+          default = pkgsUnstable.mkShell {
+
+            packages = with pkgsUnstable; [
+              jdk21
+              gradle
+              openjfx
+            ];
+
+            shellHook = ''
+              echo ""
+              echo "you have entered javaBar DevShell"
+            '';
+          };
+        }
+      );
+    };
+}
