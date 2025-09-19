ad898e5ac2cd44b293c5aab530b8ee95144836a6
JohanHaas <johanhaas99@gmail.com>
2025-09-19 17:31:21 +0200

    java

---
+++ shells/java/flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "devshell for template";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-unstable,
+  }: let
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+  in {
+    devShells = forAllSystems (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        pkgsUnstable = import nixpkgs-unstable {inherit system;};
+      in {
+        default = pkgs.mkShell {
+          packages = with pkgsUnstable; [
+            jdk21
+            gradle
+
+            jdt-language-server
+          ];
+
+          shellHook = ''
+            echo ""
+            echo "you have entered java DevShell"
+          '';
+        };
+      }
+    );
+  };
+}
